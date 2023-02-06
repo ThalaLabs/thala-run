9db90f024e66cca9fca59b0d8d7e314b2f23d262
mastercodercat <mastercoderoy64@gmail.com>
2023-02-06 07:10:35 -0500

    fixed: blank tabs for example links

--- components/Examples.tsx
+++ components/Examples.tsx
@@ -6,17 +6,16 @@ export default function Examples() {
     <Flex my={3}>
       <Text>Examples:</Text>
       <Spacer />
-      <Link as={NextLink} target="_blank" href="/?account=0x1&network=mainnet">
+      <Link as={NextLink} href="/?account=0x1&network=mainnet">
         0x1
       </Link>
       <Spacer />
-      <Link as={NextLink} target="_blank" href="/?account=0x3&network=mainnet">
+      <Link as={NextLink} href="/?account=0x3&network=mainnet">
         0x3
       </Link>
       <Spacer />
       <Link
         as={NextLink}
-        target="_blank"
         href="/?account=0x867ed1f6bf916171b1de3ee92849b8978b7d1b9e0a8cc982a3d19d535dfd9c0c&network=mainnet"
       >
         Aptos Names
