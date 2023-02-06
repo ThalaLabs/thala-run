ee300c4f11300fcac05588d5433e90586eb289cf
mastercodercat <mastercoderoy64@gmail.com>
2023-02-06 08:34:17 -0500

    fixed: account input component sync with query

--- components/AccountInput.tsx
+++ components/AccountInput.tsx
@@ -1,15 +1,24 @@
 import { Input } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { useController, useFormContext } from "react-hook-form";
 import { TxFormType } from "../lib/schema";
 
 export default function AccountInput() {
   const { control, resetField } = useFormContext<TxFormType>();
+  const router = useRouter();
+
   const {
     field: { value, onChange },
   } = useController({
     name: "account",
     control,
   });
+
+  useEffect(() => {
+    onChange(router.query.account);
+  }, [router.query.account, onChange]);
+
   return (
     <Input
       flex={1}
