3e28bfe939f1fca6ab90af34707468569a31f526
mastercodercat <mastercoderoy64@gmail.com>
2023-02-07 08:49:48 -0500

    fixed(link): removed next link from examples

--- components/Examples.tsx
+++ components/Examples.tsx
@@ -1,23 +1,15 @@
 import { Flex, Link, Spacer, Text } from "@chakra-ui/react";
-import NextLink from "next/link";
 
 export default function Examples() {
   return (
     <Flex my={3}>
       <Text>Examples:</Text>
       <Spacer />
-      <Link as={NextLink} href="/?account=0x1&network=mainnet">
-        0x1
-      </Link>
+      <Link href="/?account=0x1&network=mainnet">0x1</Link>
       <Spacer />
-      <Link as={NextLink} href="/?account=0x3&network=mainnet">
-        0x3
-      </Link>
+      <Link href="/?account=0x3&network=mainnet">0x3</Link>
       <Spacer />
-      <Link
-        as={NextLink}
-        href="/?account=0x867ed1f6bf916171b1de3ee92849b8978b7d1b9e0a8cc982a3d19d535dfd9c0c&network=mainnet"
-      >
+      <Link href="/?account=0x867ed1f6bf916171b1de3ee92849b8978b7d1b9e0a8cc982a3d19d535dfd9c0c&network=mainnet">
         Aptos Names
       </Link>
     </Flex>
