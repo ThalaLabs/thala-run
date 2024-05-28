bdd1444062cf62434aa83440031392c67f0fdfb2
shinitakunai <87251045+shinitakunai@users.noreply.github.com>
2024-05-27 22:38:33 -0700

    Simulate wip

--- components/RunCard.tsx
+++ components/RunCard.tsx
@@ -1,5 +1,10 @@
 import { useWallet } from "@aptos-labs/wallet-adapter-react";
-import { ArrowDownIcon, ArrowUpIcon, ExternalLinkIcon, SmallCloseIcon } from "@chakra-ui/icons";
+import {
+  ArrowDownIcon,
+  ArrowUpIcon,
+  ExternalLinkIcon,
+  SmallCloseIcon,
+} from "@chakra-ui/icons";
 import {
   Box,
   FormControl,
@@ -10,7 +15,9 @@ import {
   HStack,
   Spacer,
   Text,
-  Tag} from "@chakra-ui/react";
+  Tag,
+  Flex,
+} from "@chakra-ui/react";
 import { ConnectWallet } from "./ConnectWallet";
 import TypeArgsInput from "./TypeArgsInput";
 import ArgsInput from "./ArgsInput";
@@ -27,8 +34,10 @@ export function RunCard({ id }: { id: string }) {
     watch,
     moveFunc,
     onSubmit,
+    onSimualteSubmit,
     executionResult,
-    isSubmitting } = useFunctionSubmit();
+    isSubmitting,
+  } = useFunctionSubmit();
 
   const { account, network, module, func, typeArgs, args } = watch();
   const values = watch();
@@ -40,14 +49,12 @@ export function RunCard({ id }: { id: string }) {
     const idx = context.funcGroup.findIndex((x) => x.id === id);
     context.funcGroup[idx] = {
       id: id,
-      ...values
-    }
+      ...values,
+    };
     context?.setFuncGroup(context.funcGroup.slice());
-  },
-    [serializedTypeArgs, serializedArgs, id]);
+  }, [serializedTypeArgs, serializedArgs, id]);
   if (!account) return <Box></Box>;
-  if (!moveFunc)
-    return <Spinner />;
+  if (!moveFunc) return <Spinner />;
 
   // TODO: checkout https://chakra-ui.com/getting-started/with-hook-form to add errors handling
   return (
@@ -55,21 +62,37 @@ export function RunCard({ id }: { id: string }) {
       initial={{ opacity: 0, scale: 0.5 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{
-        duration: 0.3
+        duration: 0.3,
       }}
     >
       <Box
-        backgroundColor={"gray.50"} p={8} h={"fit-content"} rounded="2xl" shadow={"md"}>
-        <form onSubmit={onSubmit}>
+        backgroundColor={"gray.50"}
+        p={8}
+        h={"fit-content"}
+        rounded="2xl"
+        shadow={"md"}
+      >
+        <form>
           <FormControl>
             <HStack>
               <Heading size="sm">
-                {walletAddressEllipsis(values.account)}::{values.module}::{moveFunc.name}
-                <Tag ml={2} size="sm" colorScheme={moveFunc.is_entry ? "green" : "blue"}>{moveFunc.is_entry ? "entry" : "view"}</Tag>
+                {walletAddressEllipsis(values.account)}::{values.module}::
+                {moveFunc.name}
+                <Tag
+                  ml={2}
+                  size="sm"
+                  colorScheme={moveFunc.is_entry ? "green" : "blue"}
+                >
+                  {moveFunc.is_entry ? "entry" : "view"}
+                </Tag>
               </Heading>
               <Link
                 variant="outline"
-                href={`https://aptscan.ai/accounts/${values.account}?viewMode=${moveFunc.is_entry ? "write" : "read"}&module=${values.module}&functionName=${moveFunc.name}&network=${network}#modules`}
+                href={`https://aptscan.ai/accounts/${values.account}?viewMode=${
+                  moveFunc.is_entry ? "write" : "read"
+                }&module=${values.module}&functionName=${
+                  moveFunc.name
+                }&network=${network}#modules`}
                 isExternal
                 position="relative"
                 top="-2px"
@@ -77,39 +100,42 @@ export function RunCard({ id }: { id: string }) {
                 <ExternalLinkIcon mx="2px" />
               </Link>
               <Spacer />
-              <Button onClick={
-                () => {
+              <Button
+                onClick={() => {
                   if (!context) return;
-                  const group = context.funcGroup.slice()
+                  const group = context.funcGroup.slice();
                   const idx = group.findIndex((x) => x.id === id);
                   if (idx + 1 >= group.length) return;
 
-                  [group[idx], group[idx + 1]] = [group[idx + 1], group[idx]]
+                  [group[idx], group[idx + 1]] = [group[idx + 1], group[idx]];
                   context.setFuncGroup(group);
-                }
-              }><ArrowUpIcon />
+                }}
+              >
+                <ArrowUpIcon />
               </Button>
-              <Button onClick={
-                () => {
+              <Button
+                onClick={() => {
                   if (!context) return;
-                  const group = context.funcGroup.slice()
+                  const group = context.funcGroup.slice();
                   const idx = group.findIndex((x) => x.id === id);
                   if (idx - 1 < 0) return;
 
-                  [group[idx], group[idx - 1]] = [group[idx - 1], group[idx]]
+                  [group[idx], group[idx - 1]] = [group[idx - 1], group[idx]];
                   context.setFuncGroup(group);
-                }
-              }><ArrowDownIcon />
+                }}
+              >
+                <ArrowDownIcon />
               </Button>
-              <Button onClick={
-                () => {
+              <Button
+                onClick={() => {
                   if (!context) return;
-                  const group = context.funcGroup.slice()
+                  const group = context.funcGroup.slice();
                   const idx = group.findIndex((x) => x.id === id);
                   group.splice(idx, 1);
                   context.setFuncGroup(group);
-                }
-              }><SmallCloseIcon />
+                }}
+              >
+                <SmallCloseIcon />
               </Button>
             </HStack>
             {moveFunc.generic_type_params.length > 0 && (
@@ -120,32 +146,48 @@ export function RunCard({ id }: { id: string }) {
                 moveFunc.params.length === 1 && moveFunc.params[0] === "&signer"
               ) && <ArgsInput params={moveFunc.params} />}
             {connected || !moveFunc.is_entry ? (
-              <Button mt="2" isLoading={isSubmitting} type="submit">
-                Run
-              </Button>
+              <Flex sx={{ alignItems: "center", gap: 2 }}>
+                <Button mt="2" isLoading={isSubmitting} onClick={onSubmit}>
+                  Run
+                </Button>
+                {moveFunc.is_entry && (
+                  <Button
+                    mt="2"
+                    isLoading={isSubmitting}
+                    onClick={onSimualteSubmit}
+                  >
+                    Simulate
+                  </Button>
+                )}
+              </Flex>
             ) : (
               <ConnectWallet mt="2" />
             )}
 
-            {executionResult && <Box mt={4}>
-              {
-                moveFunc.is_entry ?
-                  <Text>Transaction:
+            {executionResult && (
+              <Box mt={4}>
+                {moveFunc.is_entry ? (
+                  <Text>
+                    Transaction:
                     <Link
                       isExternal
                       ml={2}
                       color="blue.600"
-                      href={`https://aptscan.ai/transactions/${executionResult}?network=${network}`}>{executionResult}
+                      href={`https://aptscan.ai/transactions/${executionResult}?network=${network}`}
+                    >
+                      {executionResult}
                     </Link>
-                  </Text> :
-                  <Text wordBreak={"break-word"}>Result: {executionResult}</Text>
-              }
-            </Box>}
-
+                  </Text>
+                ) : (
+                  <Text wordBreak={"break-word"}>
+                    Result: {executionResult}
+                  </Text>
+                )}
+              </Box>
+            )}
           </FormControl>
         </form>
-      </Box >
+      </Box>
     </motion.div>
   );
 }
-
--- hooks/useFunctionSubmit.tsx
+++ hooks/useFunctionSubmit.tsx
@@ -1,6 +1,13 @@
 import { useWallet } from "@aptos-labs/wallet-adapter-react";
 import { Link, useToast } from "@chakra-ui/react";
-import { AptosClient, HexString, Types } from "aptos";
+import {
+  AptosClient,
+  BCS,
+  HexString,
+  MaybeHexString,
+  TxnBuilderTypes,
+  Types,
+} from "aptos";
 import { useState } from "react";
 import { SubmitHandler, useFormContext } from "react-hook-form";
 import { TxFormType } from "../lib/schema";
@@ -10,135 +17,179 @@ import { ExternalLinkIcon } from "@chakra-ui/icons";
 import useSWR from "swr";
 
 export const useFunctionSubmit = () => {
-    const [executionResult, setExecutionResult] = useState<string>();
-    const { signAndSubmitTransaction } = useWallet();
-    const toast = useToast();
+  const [executionResult, setExecutionResult] = useState<string>();
+  const { signAndSubmitTransaction, account: walletAccount } = useWallet();
+  const toast = useToast();
 
-    const {
-        watch,
-        handleSubmit,
-        formState: { isSubmitting },
-    } = useFormContext<TxFormType>();
-    const { account, network, module, func } = watch();
+  console.log(walletAccount, "walletAccount");
 
-    const { data } = useSWR<Types.MoveModuleBytecode>(
-        [account, network, module],
-        ([account, network, module]: string[3]) =>
-            getAptosClient(network).getAccountModule(account, module)
-    );
+  const {
+    watch,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useFormContext<TxFormType>();
+  const { account, network, module, func } = watch();
 
-    const moveModule = data?.abi;
-    const moveFunc = moveModule?.exposed_functions.find((f) => f.name === func);
+  const { data } = useSWR<Types.MoveModuleBytecode>(
+    [account, network, module],
+    ([account, network, module]: string[3]) =>
+      getAptosClient(network).getAccountModule(account, module)
+  );
 
-    const _onSubmit: SubmitHandler<TxFormType> = async (data) => {
-        if (!moveFunc) return;
-        if (moveFunc.is_entry) {
-            await onSignAndSubmitTransaction(
-                network,
-                account,
-                module,
-                func,
-                data.typeArgs,
-                data.args
-            );
-        }
-        else {
-            try {
-                setExecutionResult(JSON.stringify(await getAptosClient(network)
-                    .view({
-                        function: `${account}::${module}::${func}`,
-                        type_arguments: data.typeArgs,
-                        arguments: data.args,
-                    })));
-            }
-            catch (error: any) {
-                setExecutionResult(undefined);
-                console.log("error", error);
-                toast({
-                    title: "An error occurred.",
-                    description: error.message,
-                    status: "error",
-                    duration: 5000,
-                    isClosable: true,
-                });
-            }
-        }
-    };
+  const moveModule = data?.abi;
+  const moveFunc = moveModule?.exposed_functions.find((f) => f.name === func);
 
-    async function onSignAndSubmitTransaction(
-        network: string,
-        account: string,
-        module: string,
-        func: string,
-        typeArgs: string[],
-        args: any[]
-    ) {
-        if (!moveFunc) return;
-
-        // handle params[0] === "&signer" case
-        let params = moveFunc.params;
-        if (params.length !== args.length) {
-            params = params.slice(1);
-        }
-
-        const handleArrayArgs = params.map((param, i) => {
-            const arg = args[i];
-
-            // if arg matches /vector<*>/ but not /vector<u8>/, split by comma
-            const isVector = param.match(/vector<(.*)>/);
-            if (!isVector) return arg;
-
-            const innerType = isVector[1];
-            if (innerType === "u8") return new HexString(String(arg)).toUint8Array();
-
-            return String(arg).split(",");
+  const _onSubmit: SubmitHandler<TxFormType> = async (data) => {
+    if (!moveFunc) return;
+    if (moveFunc.is_entry) {
+      await onSignAndSubmitTransaction(
+        network,
+        account,
+        module,
+        func,
+        data.typeArgs,
+        data.args
+      );
+    } else {
+      try {
+        setExecutionResult(
+          JSON.stringify(
+            await getAptosClient(network).view({
+              function: `${account}::${module}::${func}`,
+              type_arguments: data.typeArgs,
+              arguments: data.args,
+            })
+          )
+        );
+      } catch (error: any) {
+        setExecutionResult(undefined);
+        console.log("error", error);
+        toast({
+          title: "An error occurred.",
+          description: error.message,
+          status: "error",
+          duration: 5000,
+          isClosable: true,
         });
+      }
+    }
+  };
 
-        // transaction payload expects account to start with 0x
-        const account0x = account.startsWith("0x") ? account : `0x${account}`;
+  const _onSimulateSubmit: SubmitHandler<TxFormType> = async (data) => {
+    if (!moveFunc || !walletAccount) return;
 
-        const payload: Types.TransactionPayload_EntryFunctionPayload = {
-            type: "entry_function_payload",
-            function: `${account0x}::${module}::${func}`,
-            type_arguments: typeArgs,
-            arguments: handleArrayArgs,
-        };
-        try {
-            const { hash } = await signAndSubmitTransaction(payload);
-            await getAptosClient(network).waitForTransaction(hash);
+    try {
+      const client = getAptosClient(network);
+      const publicKeyUint8Array = new HexString(
+        walletAccount.publicKey as string
+      ).toUint8Array();
 
-            const href = `https://aptscan.ai/transactions/${hash}?network=${network}`;
-            setExecutionResult(hash);
+      const payload = {
+        function: `${account}::${module}::${func}`,
+        type_arguments: data.typeArgs,
+        arguments: data.args,
+      };
+      const rawTxn = await client.generateTransaction(account, payload);
 
-            toast({
-                title: "Transaction submitted.",
-                description: (
-                    <Link
-                        as={NextLink}
-                        href={href}
-                        isExternal
-                    >
-                        View on explorer <ExternalLinkIcon mx="2px" />
-                    </Link>
-                ),
-                status: "success",
-                duration: 5000,
-                isClosable: true,
-            });
-        } catch (error: any) {
-            setExecutionResult(undefined);
-            console.log("error", error);
-            toast({
-                title: "An error occurred.",
-                description: error.message,
-                status: "error",
-                duration: 5000,
-                isClosable: true,
-            });
-        }
+      setExecutionResult(
+        JSON.stringify(
+          await client.simulateTransaction(
+            new TxnBuilderTypes.Ed25519PublicKey(publicKeyUint8Array),
+            rawTxn
+          )
+        )
+      );
+    } catch (error: any) {
+      setExecutionResult(undefined);
+      console.log("error", error);
+      toast({
+        title: "An error occurred.",
+        description: error.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+  };
+
+  async function onSignAndSubmitTransaction(
+    network: string,
+    account: string,
+    module: string,
+    func: string,
+    typeArgs: string[],
+    args: any[]
+  ) {
+    if (!moveFunc) return;
+
+    // handle params[0] === "&signer" case
+    let params = moveFunc.params;
+    if (params.length !== args.length) {
+      params = params.slice(1);
     }
 
-    const onSubmit = handleSubmit(_onSubmit);
+    const handleArrayArgs = params.map((param, i) => {
+      const arg = args[i];
 
-    return { watch, executionResult, moveFunc, onSubmit, isSubmitting }
-}
+      // if arg matches /vector<*>/ but not /vector<u8>/, split by comma
+      const isVector = param.match(/vector<(.*)>/);
+      if (!isVector) return arg;
+
+      const innerType = isVector[1];
+      if (innerType === "u8") return new HexString(String(arg)).toUint8Array();
+
+      return String(arg).split(",");
+    });
+
+    // transaction payload expects account to start with 0x
+    const account0x = account.startsWith("0x") ? account : `0x${account}`;
+
+    const payload: Types.TransactionPayload_EntryFunctionPayload = {
+      type: "entry_function_payload",
+      function: `${account0x}::${module}::${func}`,
+      type_arguments: typeArgs,
+      arguments: handleArrayArgs,
+    };
+    try {
+      const { hash } = await signAndSubmitTransaction(payload);
+      await getAptosClient(network).waitForTransaction(hash);
+
+      const href = `https://aptscan.ai/transactions/${hash}?network=${network}`;
+      setExecutionResult(hash);
+
+      toast({
+        title: "Transaction submitted.",
+        description: (
+          <Link as={NextLink} href={href} isExternal>
+            View on explorer <ExternalLinkIcon mx="2px" />
+          </Link>
+        ),
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+    } catch (error: any) {
+      setExecutionResult(undefined);
+      console.log("error", error);
+      toast({
+        title: "An error occurred.",
+        description: error.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+  }
+
+  const onSubmit = handleSubmit(_onSubmit);
+  const onSimualteSubmit = handleSubmit(_onSimulateSubmit);
+
+  return {
+    watch,
+    executionResult,
+    moveFunc,
+    onSubmit,
+    onSimualteSubmit,
+    isSubmitting,
+  };
+};
